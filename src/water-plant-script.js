e7bbca971b1424eb9e3560d8077c11d62c25f695
shaharzur <shaharz@wix.com>
2021-08-24 14:17:04 +0300

    adding moisture measuring and reporting

--- src/water-plant-script.js
+++ src/water-plant-script.js
@@ -1,12 +1,29 @@
 const schedule = require('node-schedule')
 const plantWaterer = require('./plantWaterer')
 const pumpManager = require('./pumpManager')
+const moistureManager = require('./moistureManager')
+const axios = require('axios')
+const gpioNumbers = [17]
+const moistureChannels = [5]
+
+const reportMoistureLevel = ({rawValue, value, soilDrynessPercentage}, index) =>
+    axios.get(`https://shaharz.wixsite.com/my-site-3/_functions/report
+                    ?soilDrynessPercentage=${soilDrynessPercentage}
+                    &rawValue=${rawValue}
+                    &value=${value}
+                    &gpio=${gpioNumbers[index]}`
+    ).catch(err => console.log(err))
+
 
 const shouldWaterPlant = () => {
     // Run every day at 7 a.m.
     return schedule.scheduleJob('0 7 * * *', async () => {
-        return pumpManager({gpioNumbers: [17]}).getPumps().forEach(pump =>
-            plantWaterer({pumpRelay: pump, waitUntilStartTime: 3000, wateringTime: 40000}).water()
+        return pumpManager({gpioNumbers}).getPumps().forEach((pump, planterId) => {
+                moistureManager(moistureChannels[planterId]).getMoistureLevel((moistureLevel) => {
+                    reportMoistureLevel(moistureLevel, planterId)
+                    return plantWaterer({pumpRelay: pump, waitUntilStartTime: 3000, wateringTime: 40000}).water()
+                })
+            }
         )
     })
 }
