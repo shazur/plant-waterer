5d8ca605a8a283729e195ac5ec7f3fd3257c2e3c
shaharzur <shaharz@wix.com>
2021-08-29 00:18:50 +0300

    setup cron jobs - measure moisture every hour and turn on pump once in 2 days at 22

--- src/water-plant-script.js
+++ src/water-plant-script.js
@@ -4,22 +4,25 @@ const pumpManager = require('./pumpManager')
 const moistureManager = require('./moistureManager')
 const axios = require('axios')
 const gpioNumbers = [17]
-const moistureChannels = [5]
+const moistureChannels = [5, 6, 7]
 
 const reportMoistureLevel = (moistureLevel, planterId) =>
     axios.get('https://shaharz.wixsite.com/my-site-3/_functions/report', {params: {...moistureLevel, gpio: planterId}})
         .catch(err => console.log("Reporting moisture level failed", err))
 
 const shouldWaterPlant = () => {
-    // Run every day at 7 a.m.
-    return schedule.scheduleJob('0 7 * * *', async () => {
-        return pumpManager({gpioNumbers}).getPumps().forEach((pump, planterId) => {
-                moistureManager(moistureChannels[planterId]).getMoistureLevel((moistureLevel) => {
-                    reportMoistureLevel(moistureLevel, planterId)
-                    return plantWaterer({pumpRelay: pump, waitUntilStartTime: 3000, wateringTime: 40000}).water()
-                })
-            }
+    // Run every 2 days at 22:00
+    schedule.scheduleJob('0 22 */2 * *', () =>
+        pumpManager({gpioNumbers}).getPumps().forEach(pump =>
+            plantWaterer({pumpRelay: pump, waitUntilStartTime: 3000, wateringTime: 40000}).water()
         )
+    )
+    //Run every hour at minute 10 (1:10, 2:10, ...)
+    schedule.scheduledJobs({minute: 10}, () => {
+        moistureChannels.forEach((channel, planterId) =>
+            moistureManager(moistureChannels[planterId]).getMoistureLevel((moistureLevel) =>
+                reportMoistureLevel(moistureLevel, planterId)
+            ))
     })
 }
 
